1e803f348194ec01462c7f820aff8ebe68d4e761
Bs314 <104457850+Bs314@users.noreply.github.com>
2024-11-17 02:01:20 +0300

    second day1.1
    second enemy added. I added a durataion for putting some delay between two bomerang attacks

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,16 +4,35 @@ public class EnemyHealth : MonoBehaviour
 {
    
     [SerializeField] float hitPoints = 10f;
-
-    public void BomerangHits(float damagePoints)
-    {
-        hitPoints -= damagePoints;
-        if(hitPoints<=0)
-        {Destroy(gameObject);}
-
+    [SerializeField] float hitDuration = 1f;
+    bool isHitted = false;
+    float hitDurationSave;
+    private void Start() {
+        hitDurationSave = hitDuration;
     }
 
-    
-
-
+    private void Update() {
+        if(isHitted)
+        {
+            hitDuration -= Time.deltaTime;
+            if(hitDuration<=0)
+            {
+                isHitted = false;
+                hitDuration = hitDurationSave;
+            }    
+        }
+    }
+    public void BomerangHits(float damagePoints)
+    {
+        if(!isHitted)
+        {
+            hitPoints -= damagePoints;
+            if(hitPoints<=0)
+            {Destroy(gameObject);}
+            else
+            {
+                isHitted = true;
+            }
+        }
+    }
 }
